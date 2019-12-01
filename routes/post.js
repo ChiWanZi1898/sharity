85c678c5d10c9d4bc8e07f42adb680c62c56d7e9
Swarna Sathya <swarnasathya95@gmail.com>
2019-12-01 15:16:40 -0800

    * Dynamic username in jade implemented

--- routes/post.js
+++ routes/post.js
@@ -3,7 +3,10 @@ const { db } = require('../db');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('post', {data: db.data.filter(x => x.from === db.currentUser)});
+  res.render('post', {
+    data: db.data.filter(x => x.from === db.currentUser),
+    currentUser: db.currentUser
+  });
 });
 
 module.exports = router;
