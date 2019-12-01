0825d657a9391d98cd0e60387290075ed6622185
Hongyu Zhu <hongyuzh@andrew.cmu.edu>
2019-11-30 17:09:53 -0800

    fix: bug in indexing data

--- routes/item.js
+++ routes/item.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/:itemID', function(req, res, next) {
   const { itemID } = req.params;
   const {redirect} = req.query;
-  res.render('item', {data: db.data[itemID], redirect: redirect});
+  res.render('item', {data: db.data.filter(x => x.id === itemID)[0], redirect: redirect});
 });
 
 module.exports = router;
