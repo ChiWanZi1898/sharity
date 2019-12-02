d5a1f58f0041b7eceebbb68ccc3d83ded769dec3
Hongyu Zhu <hongyuzh@andrew.cmu.edu>
2019-12-01 21:41:26 -0800

    add: random requests

--- routes/confirm.js
+++ routes/confirm.js
@@ -11,6 +11,11 @@ router.post('/addNew', function (req, res, next) {
   for (let i in selected) {
     let item = JSON.parse(JSON.stringify(db.temp.find(x => x.id === selected[i])));
     item.id = `${db.data.length + 1}`;
+    for (let u in db.users) {
+      if (u !== "0" && Math.random() > 0.5) {
+        item.requested_by.push(db.users[u].name);
+      }
+    }
     db.data.push(item);
   }
   res.redirect('/post');
