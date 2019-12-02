045777fc2a523eff25b8f36e800b0becb0e2c532
devajana <devajana@andrew.cmu.edu>
2019-12-01 16:11:00 -0800

    #3

---
+++ public/javascripts/account.js
@@ -0,0 +1,15 @@
+$(function () {
+  
+    $("#home-btn").click(function () {
+      $(location).attr("href", "/home")
+    });
+  
+    $("#post-btn").click(function () {
+      $(location).attr("href", "/post")
+    });
+  
+    $("#account-btn").click(function () {
+      $(location).attr("href", "/account")
+    });
+  });
+  
